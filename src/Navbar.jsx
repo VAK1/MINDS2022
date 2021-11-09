221498db9e47fea4929d3d7e00d9b0c386724c0e
VAK1 <40782441+VAK1@users.noreply.github.com>
2021-11-09 13:52:36 -0800

    Updated Navbar to have some references

---
+++ src/Navbar.jsx
@@ -0,0 +1,16 @@
+{/* Most simple navbar I could think of */}
+
+<Navbar bg="dark" variant="dark" fixed="top">
+  <Container>
+  <Navbar.Brand href="#home">'MINDS 2022'</Navbar.Brand>
+  <Nav className="me-auto">
+    <Nav.Link href="#mission">Mission</Nav.Link>
+    <Nav.Link href="#whyjoin">'Why Join?'</Nav.Link>
+    <Nav.Link href="#events">Events</Nav.Link>
+    <Nav.Link href="#timeline">Timeline</Nav.Link>
+    <Nav.Link href="#resources">Resources</Nav.Link>
+    <Nav.Link href="#about">About us</Nav.Link>
+    <Nav.Link href="#register">Register</Nav.Link>
+  </Nav>
+  </Container>
+</Navbar>
