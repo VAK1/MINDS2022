137a3afff23c6d7d1ab5e97b5854541b41b65bd7
Vikram Khandelwal <vikram.a.khandelwal@gmail.com>
2021-11-13 15:15:08 -0800

    Marginer and Page Container

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>MINDS 2022</h1>
-    </div>
+      <div className="App">
+        <h1>MINDS 2022</h1>
+      </div>
   );
 }
 
